793fe93d46a2f10c1f9bc6f3c0756957359cb14a
Soweleq <162826757+Soweleq@users.noreply.github.com>
2025-06-03 15:47:47 +0200

    build

---
+++ app.js
@@ -0,0 +1,25 @@
+const app = angular.module('portfolioApp', []);
+
+app.controller('MainController', function ($scope, $http) {
+  $scope.lang = localStorage.getItem("lang") || "plhome";
+  $scope.showCookies = localStorage.getItem("cookiesAccepted") !== "yes";
+
+  $scope.loadTranslations = function (lang) {
+    $http.get(`i18n/${lang}.json`).then((res) => {
+      $scope.t = res.data;
+    });
+  };
+
+  $scope.changeLang = function (lang) {
+    localStorage.setItem("lang", lang);
+    $scope.loadTranslations(lang);
+  };
+
+  $scope.acceptCookies = function () {
+    localStorage.setItem("cookiesAccepted", "yes");
+    $scope.showCookies = false;
+  };
+
+  $scope.loadTranslations($scope.lang);
+
+});
